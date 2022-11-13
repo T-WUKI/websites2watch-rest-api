7939a25ab306eaecc388884e0a5b9ca276951434
Thomas Wukasch <thomaswukasch@MacBook-Pro-von-Thomas.fritz.box>
2022-11-13 23:24:14 +0100

    Setup project

---
+++ src/main/java/com/wuki/websites2watch/Websites2watchApplication.java
@@ -0,0 +1,13 @@
+package com.wuki.websites2watch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Websites2watchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Websites2watchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wuki/websites2watch/Websites2watchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wuki.websites2watch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Websites2watchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
