7a44ab870762dffc0e41eb4c30666f905406f99c
Thomas Wukasch <thomaswukasch@MacBook-Pro-von-Thomas.fritz.box>
2022-11-20 19:41:13 +0100

    WebsiteRepository refactored

--- src/main/java/com/wuki/websites2watch/repository/WebsiteRepository.java
+++ src/main/java/com/wuki/websites2watch/repository/WebsiteRepository.java
@@ -8,31 +8,34 @@ import java.util.List;
 
 public class WebsiteRepository {
 
+  private List<WebsiteResponse> websites = Arrays.asList(
+    new WebsiteResponse(
+      "bg-glattal",
+      "https://www.bg-glattal.ch/vermietung/",
+      "first website to watch",
+      Arrays.asList("Genossenschaft", "Miete"),
+      Arrays.asList("watchSingleChanges"),
+      Arrays.asList("Zürich Schwamendingen")
+    ),
+    new WebsiteResponse(
+      "bsh-zürich",
+      "https://www.bsh-zuerich.ch/freie-wohnungen/",
+      "second website to watch",
+      Arrays.asList("Genossenschaft", "Miete"),
+      Arrays.asList("watchSingleChanges"),
+      Arrays.asList("Zürich Frankental")
+    )
+  );
+
   public List<WebsiteResponse> findAll(String tag, String region) {
-    List<WebsiteResponse> result = new ArrayList<>();
-    result.addAll(
-      Arrays.asList(
-        new WebsiteResponse(
-          "bg-glattal",
-          "https://www.bg-glattal.ch/vermietung/",
-          "first website to watch",
-          Arrays.asList("Genossenschaft", "Miete"),
-          Arrays.asList("watchSingleChanges"),
-          Arrays.asList("Zürich Schwamendingen")
-        ),
-        new WebsiteResponse(
-          "bsh-zürich",
-          "https://www.bsh-zuerich.ch/freie-wohnungen/",
-          "second website to watch",
-          Arrays.asList("Genossenschaft", "Miete"),
-          Arrays.asList("watchSingleChanges"),
-          Arrays.asList("Zürich Frankental")
-        )
-      )
-    );
-    result = result.stream().filter(r -> tag == null || r.getTags().stream().map(String::toLowerCase).toList().contains(tag.toLowerCase())).toList();
-    result = result.stream().filter(r -> region == null || r.getRegions().stream().map(String::toLowerCase).toList().contains(region.toLowerCase())).toList();
+    List<WebsiteResponse> result = new ArrayList<>(websites);
+    result = result.stream().filter(r -> tag == null || getLowerCaseStrings(r.getTags()).contains(tag.toLowerCase())).toList();
+    result = result.stream().filter(r -> region == null || getLowerCaseStrings(r.getRegions()).contains(region.toLowerCase())).toList();
     return result;
+  }
+
+  private static List<String> getLowerCaseStrings(List<String> r) {
+    return r.stream().map(String::toLowerCase).toList();
   }
 
 
